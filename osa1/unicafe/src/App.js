268bc579c044c73b7e42044b94bdb590a431abe7
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-02-15 16:31:39 +0200

    Tehtava 1.7 unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -25,13 +25,32 @@ const Stats = (props) => {
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0) 
+    const [bad, setBad] = useState(0)
     
     const handleClick = (setter, value, text) => {
         const handler = () => {
             setter(value + 1)
         }
         return handler
+    }
+
+    const allCalculator = () => {
+        const all = good+neutral+bad
+        return all
+    }
+
+    const averageCalculator = () => {
+        const all = allCalculator()
+        const sum = good - bad
+        const average = sum/all
+        return average
+    }
+
+    const positiveCalculator = () => {
+        const all = allCalculator()
+        const percentage = (good/all)*100
+        const percentagestring = percentage.toString() + ' %'
+        return percentagestring
     }
 
     return (
@@ -44,6 +63,9 @@ const App = () => {
             <Stats text={ 'Good' } value={ good } />
             <Stats text={ 'Neutral' } value={ neutral } />
             <Stats text={ 'Bad' } value={ bad } />
+            <Stats text={ 'All' } value={ allCalculator() } />
+            <Stats text={ 'Average' } value={ averageCalculator() } />
+            <Stats text={ 'Positive' } value={ positiveCalculator() } />
         </div>
     )
 }
