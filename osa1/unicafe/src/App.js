5cc7dad1fb6420357fe0bf6445583a0106e2a0fd
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-02-16 09:45:43 +0200

    Tehtava 1.8 unicafe step3

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -16,6 +16,45 @@ const Button = (props) => {
     )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+    const allCalculator = () => {
+        const all = good+neutral+bad
+        return all
+    }
+
+    const averageCalculator = () => {
+        const all = allCalculator()
+        if (all === 0) {
+            return 0
+        }
+        const sum = good - bad
+        const average = sum/all
+        return average
+    }
+
+    const positiveCalculator = () => {
+        const all = allCalculator()
+        if (all === 0) {
+            return '0 %'
+        }
+        const percentage = (good/all)*100
+        const percentagestring = percentage.toString() + ' %'
+        return percentagestring
+    }
+
+    return (
+        <div>
+            <Title title={ 'Statistics' } />
+            <Stats text={ 'Good' } value={ good } />
+            <Stats text={ 'Neutral' } value={ neutral } />
+            <Stats text={ 'Bad' } value={ bad } />
+            <Stats text={ 'All' } value={ allCalculator() } />
+            <Stats text={ 'Average' } value={ averageCalculator() } />
+            <Stats text={ 'Positive' } value={ positiveCalculator() } />
+        </div>
+    )
+}
+
 const Stats = (props) => {
     return (
         <p>{ props.text }: { props.value }</p>
@@ -26,31 +65,12 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    
+
     const handleClick = (setter, value, text) => {
         const handler = () => {
             setter(value + 1)
         }
         return handler
-    }
-
-    const allCalculator = () => {
-        const all = good+neutral+bad
-        return all
-    }
-
-    const averageCalculator = () => {
-        const all = allCalculator()
-        const sum = good - bad
-        const average = sum/all
-        return average
-    }
-
-    const positiveCalculator = () => {
-        const all = allCalculator()
-        const percentage = (good/all)*100
-        const percentagestring = percentage.toString() + ' %'
-        return percentagestring
     }
 
     return (
@@ -59,13 +79,7 @@ const App = () => {
             <Button clickHandler={handleClick(setGood, good, 'good')} text={'good'} />
             <Button clickHandler={handleClick(setNeutral, neutral, 'neutral')} text={'neutral'} />
             <Button clickHandler={handleClick(setBad, bad, 'bad')} text={'bad'} />
-            <Title title={ 'Statistics' } />
-            <Stats text={ 'Good' } value={ good } />
-            <Stats text={ 'Neutral' } value={ neutral } />
-            <Stats text={ 'Bad' } value={ bad } />
-            <Stats text={ 'All' } value={ allCalculator() } />
-            <Stats text={ 'Average' } value={ averageCalculator() } />
-            <Stats text={ 'Positive' } value={ positiveCalculator() } />
+            <Statistics good={ good } neutral={ neutral } bad={ bad }/>
         </div>
     )
 }
